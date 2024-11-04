2b709d3e096708babe6fcaadbdc45d318e485331
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2024-11-04 11:50:46 +0530

    added datek and spot less

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
+import com.diffplug.gradle.spotless.SpotlessPlugin
+import io.gitlab.arturbosch.detekt.DetektPlugin
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.spotless)
+}
+
+configure(subprojects) {
+    apply<DetektPlugin>()
+    apply<SpotlessPlugin>()
+
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            ktlint("0.43.2")
+        }
+    }
 }
